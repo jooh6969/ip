b2a0c09662a8ff13ad20bec9b674c62a62be542b
jooh6969 <xlorddopist@gmail.com>
2025-08-22 16:51:58 +0800

    Completed Level-1

--- src/main/java/Jooh.java
+++ src/main/java/Jooh.java
@@ -1,19 +1,29 @@
+import java.util.Scanner;
+
 public class Jooh {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         String line = "____________________________________________________________________________________";
         System.out.println(line);
         System.out.println("Hello! I'm Jooh, a pre-programmed ChatBot");
         System.out.println("What can I do for you today?");
         System.out.println(line);
 
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
+        while(true) {
+            String input = sc.nextLine();
+            if (input.equalsIgnoreCase("bye")) {
+                System.out.println(line);
+                System.out.println("Have a nice day! Goodbye!");
+                System.out.println(line);
+                break;
+            } else {
+                System.out.println(line);
+                System.out.println(input);
+                System.out.println(line);
+            }
         }
+        sc.close();
 
-        System.out.println(line);
-        System.out.println("Have a nice day! Goodbye!");
-        System.out.println(line);
+
     }
 }
