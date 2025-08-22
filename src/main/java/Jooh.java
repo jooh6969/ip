68bbfeaf0871408254c8ec74f765a48fee92ad30
jooh6969 <xlorddopist@gmail.com>
2025-08-22 18:33:35 +0800

    Level-2

--- src/main/java/Jooh.java
+++ src/main/java/Jooh.java
@@ -1,8 +1,11 @@
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.stream.IntStream;
 
 public class Jooh {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        ArrayList<String> str = new ArrayList<>();
         String line = "____________________________________________________________________________________";
         System.out.println(line);
         System.out.println("Hello! I'm Jooh, a pre-programmed ChatBot");
@@ -16,9 +19,15 @@ public class Jooh {
                 System.out.println("Have a nice day! Goodbye!");
                 System.out.println(line);
                 break;
-            } else {
+            } else if (input.equalsIgnoreCase("list")) {
                 System.out.println(line);
-                System.out.println(input);
+                IntStream.range(0, str.size())
+                        .forEach(i -> System.out.println(i + 1 + ": " + str.get(i)));
+                System.out.println(line);
+            } else {
+                str.add(input);
+                System.out.println(line);
+                System.out.println("added: " + input);
                 System.out.println(line);
             }
         }
